f8d92af62069aeb156c2c83cd6b831ffb7e3f632
Tavishi Singh <tavishisinghx@gmail.com>
2020-04-30 02:25:57 +0530

    Final Commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,5 +4,35 @@
  */
 package main;
 
+import BinaryTree.MyBinarySearchTreeUsingLinkedList;
+
 public class Main {
+    public static void main(String[] args) {
+        MyBinarySearchTreeUsingLinkedList bst = new MyBinarySearchTreeUsingLinkedList();
+        bst.insert(50);
+        bst.insert(25);
+        bst.insert(75);
+        bst.insert(10);
+        bst.insert(35);
+        bst.insert(40);
+        System.out.println("Height of tree");
+        System.out.println(bst.height(bst.getRoot()));
+        System.out.println("Height of tree");
+        System.out.println(bst.heightNonRecursive(bst.getRoot()));
+        System.out.println("InOrder Traversal");
+        bst.traverseInOrder(bst.getRoot());
+        System.out.println();
+        System.out.println("InOrder Traversal");
+        bst.traverseInOrder(bst.getRoot());
+        System.out.println();
+        System.out.println("PreOrder Traversal");
+        bst.traversePreOrder(bst.getRoot());
+        System.out.println();
+        System.out.println("PostOrder Traversal");
+        bst.traversePostOrder(bst.getRoot());
+        boolean result=bst.search(40);
+        if(result==true) System.out.println("element found");
+        else System.out.println("element not found");
+
+    }
 }
