dae201650d9a1686326736021bef1dfc03e6b828
Tavishi Singh <tavishisinghx@gmail.com>
2020-04-30 01:50:15 +0530

    Final Commit

--- src/BinaryTree/MyBinarySearchTreeUsingLinkedList.java
+++ src/BinaryTree/MyBinarySearchTreeUsingLinkedList.java
@@ -4,5 +4,38 @@
  */
 package BinaryTree;
 
+import node.TreeNode;
+
 public class MyBinarySearchTreeUsingLinkedList {
+    private TreeNode root;
+
+    public TreeNode getRoot() {
+        return root;
+    }
+    public void insert(int data){
+        TreeNode node = new TreeNode(data);
+
+        if(root == null){
+            root = node;
+        }
+        else{
+            TreeNode temp = root;
+            TreeNode parent = null;
+            while (temp != null){
+                parent = temp;
+                if(node.getData() <= temp.getData()){
+                    temp = temp.getLeft();
+                }
+                else{
+                    temp = temp.getRight();
+                }
+            }
+            if(node.getData() <= parent.getData()){
+                parent.setLeft(node);
+            }
+            else {
+                parent.setRight(node);
+            }
+        }
+    }
 }
