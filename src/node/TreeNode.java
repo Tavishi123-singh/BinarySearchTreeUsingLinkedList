514268ab6f8d1a34e90dbdf1d3d4ec8182e0f6ee
Tavishi Singh <tavishisinghx@gmail.com>
2020-04-30 01:40:08 +0530

    Final Commit

--- src/node/TreeNode.java
+++ src/node/TreeNode.java
@@ -5,4 +5,47 @@
 package node;
 
 public class TreeNode {
+    private int data;
+    private TreeNode left;
+    private TreeNode right;
+    private int height;
+
+    public TreeNode(int data) {
+        this.data = data;
+        left=null;
+        right=null;
+        height=0;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
 }
